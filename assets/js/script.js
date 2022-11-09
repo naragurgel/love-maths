ecc9ebe6508184f4f3cd7fa2ee542eb3b0ba993e
naragurgel <gurgel.nara@gmail.com>
2022-11-09 20:04:52 +0000

    Add code to increment acore and incorret answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,8 +42,10 @@ function runGame(gameType) {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -65,9 +67,24 @@ function calculateCorrectAnswer() {
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
 }	
-function incrementScore() {	
-}	
-function incrementWrongAnswer() {	
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+ function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+
+}
+
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+    
 }	
 function displayAdditionQuestion(operand1, operand2) {	
     document.getElementById('operand1').textContent = operand1;	
